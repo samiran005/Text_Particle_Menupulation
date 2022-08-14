f131dafd7a8eede22da7183af2becd30ad653f19
samiran005 <maitysamiran442@gmail.com>
2022-08-14 13:14:06 +0530

    Text perticle menupuletion

--- main.js
+++ main.js
@@ -21,3 +21,57 @@ window.addEventListener("mousemove", (event)=>{
 ctx.fillStyle = "white";
 ctx.font = "30px verdana";
 ctx.fillText("A", 0, 50);
+const data = ctx.getImageData(0,0,100,100)
+
+
+//particle creation class
+class Perticle {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.radius = 3;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) + 1
+    }
+    draw(){
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if(distance < 100){
+            this.radius = 10;
+        }else {
+            this.radius = 3;
+        }
+    }
+}
+
+//position of the peritcle
+function init() {
+    particleArray = [];
+    for(i=0;i<1000;i++){
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        particleArray.push(new Perticle(x,y))
+    }
+} 
+init();
+
+//draw perticle
+function animate() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for(i = 0; i < particleArray.length; i++){
+        particleArray[i].draw();
+        particleArray[i].update();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
